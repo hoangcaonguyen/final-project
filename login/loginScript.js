47f18a22814579f9d4f241f124ea6903c1d4859a
hoang cao nguyen <hoangcaonguyen1999@gmail.com>
2022-02-12 19:44:05 +0700

    updateProject

---
+++ login/loginScript.js
@@ -0,0 +1,32 @@
+const myForm = document.getElementById("wrapper");
+
+let account = {
+    email: 'hoangcaonguyen1999@gmail.com',
+    fullName: 'Hoàng Cao Nguyên',
+    passWord: 'hoangcaonguyen1999',
+    phoneNumber: '0987764770',
+    address: 'hà đông, hà nội',
+    dateOfBirth: '31/07/1999',
+};
+
+myForm.addEventListener("submit", function(event) {
+    let email = document.getElementById("email").value;
+    let passWord = document.getElementById("pass").value;
+    if (email == "" && passWord == "") {
+        alert(" chua nhap tai khoan mat khau!");
+    } else {
+        if (passWord.length > 8) {
+            if (email.includes(" ") || passWord.includes(" ")) {
+                alert("khong duoc chua space!!");
+            } else {
+                if (email === account.email && passWord === account.passWord) {
+                    alert(" dang nhap thanh cong !");
+                } else {
+                    alert(" sai user name hoac password !");
+                }
+            }
+        } else if (passWord.length < 8) {
+            alert(" mat khau phai 8 ky tu tro len");
+        }
+    }
+});
